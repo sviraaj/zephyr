8ab610d59a24b5f7112d75933b254c5a2703ef47
Zedblox_Firmware <ykumarg7@gmail.com>
2026-07-16 16:05:34 +0530

    changes sometimes USB33RDY is still 0, PWR->CR3=0x01000042 issue sometimes no issues

--- drivers/usb/device/usb_dc_stm32.c
+++ drivers/usb/device/usb_dc_stm32.c
@@ -396,7 +396,7 @@ static int usb_dc_stm32_init(void)
 	/* Per AN2606: USBREGEN not supported when running in FS mode. */
 	LL_PWR_DisableUSBReg();
 	while (!LL_PWR_IsActiveFlag_USB()) {
-		LOG_INF("PWR not active yet");
+		printk("USB33RDY is still 0, PWR->CR3=0x%08x\n", PWR->CR3);
 		k_sleep(K_MSEC(100));
 	}
 #endif
